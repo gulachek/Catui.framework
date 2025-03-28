1ab9247a2a03ac0fe69f531cf4c671fafa9386a3
Nicholas Gulachek <nsgulachek@gmail.com>
2025-03-27 21:40:36 -0500

    Initial Commit

---
+++ Catui/Catui.h
@@ -0,0 +1,18 @@
+//
+//  Catui.h
+//  Catui
+//
+//  Created by Nicholas Gulachek on 3/27/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Catui.
+FOUNDATION_EXPORT double CatuiVersionNumber;
+
+//! Project version string for Catui.
+FOUNDATION_EXPORT const unsigned char CatuiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Catui/PublicHeader.h>
+
+
