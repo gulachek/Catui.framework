e2fe3ee115c2dbc4fd585381588904bbd183528f
Nicholas Gulachek <nsgulachek@gmail.com>
2025-03-31 07:10:31 -0500

    Add doxygen comments

--- Catui/CatuiSemver.h
+++ Catui/CatuiSemver.h
@@ -8,15 +8,47 @@
 #ifndef CatuiSemver_h
 #define CatuiSemver_h
 
+/**
+ * Represents a semantic version as applied in the catui protocol
+ */
 @interface CatuiSemver : NSObject
 
+/**
+ * The major version component
+ */
 @property (readonly) uint16_t major;
+
+/**
+ * The minor version component
+ */
 @property (readonly) uint16_t minor;
+
+/**
+ * The patch version component
+ */
 @property (readonly) uint32_t patch;
 
+/**
+ * Initialize with numeric version components
+ * @param major The majoor version component
+ * @param minor The minor version component
+ * @param patch The patch version component
+ * @returns The initialized instance
+ */
 - (id)initWithMajor:(uint16_t)major minor:(uint16_t)minor patch:(uint32_t)patch;
 
+/**
+ * Check if the instance's desired version can use another API's version according to semantic versioning conventions
+ * @param api The other API's version potentially supporting this instance's desired version
+ * @returns YES if this instance's desired version can use the API's version
+ */
 - (BOOL)canUse:(CatuiSemver *)api;
+
+/**
+ * Check if the instance's version can support another consumer's desired API version according to semantic versioning conventions
+ * @param consumer The other consumer's desired API version potentially being supported by this instance's API version
+ * @returns YES if this instance's API version can support the consumer's desired API version
+ */
 - (BOOL)canSupport:(CatuiSemver *)consumer;
 
 @end
