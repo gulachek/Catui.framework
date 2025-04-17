306468b10cc68241892ae93562b9fd6737a0a10d
Nicholas Gulachek <nsgulachek@gmail.com>
2025-04-17 08:59:03 -0500

    Assert licenses exist in bundle

--- script/test-pkg.sh
+++ script/test-pkg.sh
@@ -14,6 +14,16 @@ xcodebuild -create-xcframework \
 	-framework build/Release/Catui.framework \
 	-output Catui.xcframework
 
+# Make sure we've included licenses
+for RES in Catui.xcframework/*/Catui.framework/RESOURCES; do
+	test -f "$RES/LICENSE"
+	DEP="$RES/DEPENDENCY-LICENSES"
+	test -f "$DEP/catui.txt"
+	test -f "$DEP/msgstream.txt"
+	test -f "$DEP/unixsocket.txt"
+	test -f "$DEP/cJSON.txt"
+done
+
 # Now test basic Swift package referencing above
 cd testpkg
 swift run
